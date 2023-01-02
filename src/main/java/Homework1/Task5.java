711026192543755002f231234ba38b946e19f6bd
Stiliyan Ignatov (Signatov) <ignatov.stili@gmail.com>
2023-01-02 11:46:45 +0000

    UPDATE DIRECTORY!! Task1 Task2 Task3 Task4 Task5

---
+++ src/main/java/Homework1/Task5.java
@@ -0,0 +1,16 @@
+package Homework1;
+
+public class Task5
+{
+    public static void main(String[] Args) {
+        int a;            //типа променливи от целочислен
+        int b;
+        for (a = 1; a <= 100; a++) { //определяне  границите на извършеният цикъл
+            System.out.println(a); // извеждане в конзолата резултата!
+        }
+        for (b = 100; b >= 1; b--) {
+            System.out.println( b );
+        }
+    }
+}
+
